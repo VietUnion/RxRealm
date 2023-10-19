5caaa3b76b53dcabf9427f5662112deb1d691538
Truc Hoang <truc.hoang@vietunion.com.vn>
2023-10-19 17:59:08 +0700

    fix ambiguity between Foundation's Observable and RxSwift's Observable in xcode 15

---
+++ Sources/RxRealm/Observable+Alias.swift
@@ -0,0 +1,11 @@
+//
+//  Observable+Alias.swift
+//  RxRealm
+//
+//  Created by truc.hoang on 19/10/2023.
+//  Copyright © 2023 RxSwiftCommunity. All rights reserved.
+//
+
+import RxSwift
+
+typealias Observable = RxSwift.Observable
